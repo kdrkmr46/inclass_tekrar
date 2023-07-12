a35ed1b2b18969bb402ceb6872d55620cbab003b
kdrkmr46 <kdrkmr46@gmail.com>
2023-07-12 11:45:56 +0300

    basketball

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,5 +1,26 @@
-const PlayerCard = () => {
-  return <div>PlayerCard</div>;
+import { useState } from "react";
+
+import Card from "react-bootstrap/Card";
+
+const PlayerCard = ({ name, img, statistics }) => {
+  const [showİmage, setShowİmage] = useState(true);
+  const handleClick = () => setShowİmage(!showİmage);
+  return (
+    <Card onClick={handleClick} role="button">
+      {showİmage ? (
+        <Card.Img variant="top" src={img} />
+      ) : (
+        <ul>
+          {statistics.map((item, i) => {
+            return <li key={i}> 🏀{item} </li>;
+          })}
+        </ul>
+      )}
+      <Card.Body>
+        <Card.Title> {name} </Card.Title>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default PlayerCard;
